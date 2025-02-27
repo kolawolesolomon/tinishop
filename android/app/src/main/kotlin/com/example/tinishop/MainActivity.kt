561629a179dfe842d54fa8a30d459c9cf5a666a8
kolichu12 <kolichu12@gmail.com>
2025-02-27 07:27:33 +0100

    This is the first version of the app with just an emptyh bottomNavigationBar and scaffold widget

---
+++ android/app/src/main/kotlin/com/example/tinishop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tinishop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/bnb.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BnbWidget extends StatefulWidget {
+  const BnbWidget({super.key});
+
+  @override
+  State<BnbWidget> createState() => _BnbWidgetState();
+}
+
+class _BnbWidgetState extends State<BnbWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
